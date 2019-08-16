814f8977065b7ad254ba1c7aae26f7dc4e1bcc14
Devan Skeem <devanskeem@gmail.com>
2019-08-16 00:40:24 -0600

    optimized bubble sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,10 +1,15 @@
 function bubbleSort(array){
     for (let i = array.length - 1; i >= 0; i--){
+        let swap = false;
+        console.log('array', array)
         for (let j = 0; j < i; j++){
-            if(array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]]
+            if(array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
+                swap = true;
+            }
         }
+        if (!swap) return array
     }
-    return array
 }
 
-console.log(bubbleSort([3,1,2,4,42,23,462,234,2345,2123,132,12,245,12,454,245,125,25121]))
+console.log(bubbleSort([3,1,234,234523,23452,233,45,6763,45,89,576]))
